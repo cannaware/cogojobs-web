f0946a78be9b6630bf5a884d7b5903afeb77a96c
cannawaredev <cannaware.io@gmail.com>
2021-10-07 14:54:56 -0300

    add support for theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/kanit/400.css';
+import '@fontsource/kanit/600.css';
 import type { AppProps } from 'next/app';
 
-import '../../styles/globals.css';
-
-// import theme from '../utils/fonts';
+import theme from '../utils/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
-import { Center } from '@chakra-ui/layout';
+import { Center, Container, Stack, Text, Heading } from '@chakra-ui/layout';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <Center bg="green.500" h="100px" color="white" fontFamily="Kanit" fontSize="xx-large">
-      cogojobs
-    </Center>
+    <Container>
+      <Stack>
+        <Heading color="green.400">The spectacle before us was indeed sublime.</Heading>
+        <Text>
+          Apparently we had reached a great height in the atmosphere, for the sky was a dead black,
+          and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the
+          sea to the level of the spectator on a hillside, the sable cloud beneath was dished out,
+          and the car seemed to float in the middle of an immense dark sphere, whose upper half was
+          strewn with silver. Looking down into the dark gulf below, I could see a ruddy light
+          streaming through a rift in the clouds.
+        </Text>
+      </Stack>
+      <Center bg="green.400" h="100px" color="white" fontSize="xx-large">
+        cogojobs
+      </Center>
+    </Container>
   );
 };
 
