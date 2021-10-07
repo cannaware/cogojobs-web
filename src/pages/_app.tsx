e6c47970ff43d7d15b1f6f788dccec342b8a41f1
cannawaredev <cannaware.io@gmail.com>
2021-10-07 01:24:15 -0300

    add demo theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 import '../../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// import theme from '../utils/fonts';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp;
+export default App;
---
+++ src/utils/fonts.tsx
@@ -0,0 +1,11 @@
+import { Global } from '@emotion/react';
+
+const Fonts = () => (
+  <Global
+    styles={`
+      @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@400;600;700&display=swap');
+    `}
+  />
+);
+
+export default Fonts;
