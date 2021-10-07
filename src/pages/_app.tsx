10073258819787fa99be3acaea01ac56ba84f1da
cannawaredev <cannaware.io@gmail.com>
2021-10-07 14:57:34 -0300

    add support for module aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import '@fontsource/kanit/400.css';
 import '@fontsource/kanit/600.css';
 import type { AppProps } from 'next/app';
 
-import theme from '../utils/theme';
+import theme from '@/utils/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
