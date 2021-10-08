d946a5103696e26eb715a02e9c5c04e4021c3755
cannawaredev <cannaware.io@gmail.com>
2021-10-07 22:35:40 -0300

    add initial title

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Flex } from '@chakra-ui/layout';
+import Head from 'next/head';
 import type { ReactElement } from 'react';
 
 type LayoutProps = {
@@ -7,9 +8,14 @@ type LayoutProps = {
 
 const Layout = ({ children, ...props }: LayoutProps) => {
   return (
-    <Flex direction="column" maxW={{ xl: '1200px' }} m="0 auto" p={6} {...props}>
-      {children}
-    </Flex>
+    <>
+      <Head>
+        <title>Cogojobs</title>
+      </Head>
+      <Flex direction="column" maxW={{ xl: '1200px' }} m="0 auto" p={6} {...props}>
+        {children}
+      </Flex>
+    </>
   );
 };
 
