fc891fde4e72e26fdb5bf76b850379ffdb6fefa1
cannawaredev <cannaware.io@gmail.com>
2021-10-11 15:03:41 -0300

    add fake imagge loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    // INFO: fake loader just a hack to avoid the build error with CF Pages
+    loader: 'imgix',
+    path: '',
+  },
+};
