b534213cbc5c49ea7a74bbff3e066425db8caec7
cannawaredev <cannaware.io@gmail.com>
2021-10-11 18:30:12 -0300

    germinando 🌱

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -2,7 +2,7 @@
 import Image, { ImageLoaderProps, ImageProps } from 'next/image';
 
 const DEFAULT_QUALITY = 75;
-const CF_IMAGES_WORKER_URL = 'https://image-resize.cannaware.workers.dev';
+const CF_IMAGES_WORKER_URL = 'https://images.cannaware.workers.dev';
 const SITE_URL = 'https://cogojobs.com';
 
 const cloudflareImageLoader = ({ src, width, quality = DEFAULT_QUALITY }: ImageLoaderProps) => {
