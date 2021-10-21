87e0b7270de8bd38471cf881a1c2bcbd7b73af9d
bycolco <bycolco@gmail.com>
2021-10-21 01:16:13 -0300

    update page title

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,7 @@ function Layout({ children }: LayoutProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <title>Cogojobs - Cultiva tu talento</title>
+        <title>Cogojobs - Cultivá tu talento</title>
       </Head>
       <Header />
       {children}
